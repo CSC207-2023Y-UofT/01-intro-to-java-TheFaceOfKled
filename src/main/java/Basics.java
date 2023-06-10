d27e11c1285d9b02e1697aa99cf70ae0ea4998e0
Henry <henry.ren@mail.utoronto.ca>
2023-06-10 00:59:40 -0400

    Edited

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -2,7 +2,6 @@
  * This file contains a few exercises to familiarize you with Java.
  * You should read this file from top-to-bottom. Any tasks you are to complete
  * are labelled with TODO
- *
  * For your convenience, we have also included references to the
  * relevant readings for each task.
  */
@@ -10,9 +9,7 @@
 /**
  *  1. In Java, no code can exist outside of a class. Unlike Python which uses
  *    functions, all code in Java uses methods.
- *
  *    For this exercise, all of our code is within the Basics class.
- *
  *    (Relevant reading: 1.1.1. Defining classes)
  */
 public class Basics {
@@ -25,10 +22,8 @@ public class Basics {
      *        public static void main(final String[] args){
      *            // Code goes in here
      *        }
-     *
      *    When you run this file (Basics.java), the code within the main
      *    method is run.
-     *
      *    (Relevant reading: 1.1.2. Defining methods)
      */
     public static void main(final String[] args) {
@@ -55,7 +50,6 @@ public class Basics {
          */
 
         int my_variable = 100;
-
 
         /* Do not remove the line below: if you did task 2 correctly, then
          * you should see the message "my_variable's value is 100" if you run
@@ -98,24 +92,18 @@ public class Basics {
      *  6. Below is a method named split which returns a String,
      *    as indicated by the 'String' part of the header. It takes one
      *    parameter named to_split, whose type is also a String.
-     *
      *    This block of code above the method is called the doc comment -- this
      *    is equivalent to docstrings in Python.
-     *
      *    Below, we provide descriptions of what the parameters are and details
      *    about what is returned.
-     *
      * Create a String named 'ret' and get the first letter of
      * each word inside to_string. to_string is a String containing exactly
      * 7 words, separated by spaces.
-     *
      * You may want to use String.split() and String.charAt() methods.
      * See 1.5.2. APIs for their APIs.
-     *
      * For example, calling the below method on the string:
      *    "Idol Long Oolong Vertical Europe University Toyota"
      * should return "ILOVEUT"
-     *
      * (Relevant readings: 1.4. Strings and 1.4.4. StringBuilder.
      *                     1.8.2. for Loops may be useful too.)
      *
@@ -141,16 +129,13 @@ public class Basics {
         return ret.toString();
     }
 
-
     /**
      * 7. Below is a method that takes in an integer array (similar to a list
      *    of integers). The method's name is 'oddSum' and it returns an int
      *    as indicated by the 'int' in the method header.
-     *
      * Return the sum of all integers at odd indices in the given array.
      * If there are no items at odd indices (e.g. a list with 0 or 1 elements),
      * return 0.
-     *
      * (Relevant readings: 1.6. Arrays and 1.8.2. for Loops)
      *
      * @param arr    An array of integers
@@ -158,20 +143,11 @@ public class Basics {
      */
     public static int oddSum(int[] arr) {
         int current_sum = 0;
-        for (int i = 1; i < arr.length; i += 2){
-            current_sum += arr[i];
+        if (arr.length != 0 && arr.length != 1){
+            for (int i = 1; i < arr.length; i += 2) {
+                current_sum += arr[i];
+            }
         }
         return current_sum;
     }
-
-
-    /*
-     * TODO (Task 6): Submit the changes you made on GitHub!
-     *                When you submit it, go to the 'Actions' tab. You should
-     *                be able to see this exercise being autograded!
-     *
-     *                You can click on the ongoing action to see more details.
-     *                If the tests don't pass, look at the results and revise
-     *                accordingly.
-     */
 }
